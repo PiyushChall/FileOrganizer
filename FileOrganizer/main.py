6fbe15cd17bb1531229aef5f4da24b5d153ac16a
PiyushChall <challpiyush79@gmail.com>
2024-04-25 20:20:57 +0530

    Added GUI

--- FileOrganizer/main.py
+++ FileOrganizer/main.py
@@ -1,9 +1,19 @@
 import os
+import tkinter
+import customtkinter
 import shutil
 import json
 
 with open("Categories.json") as f:
     categories = json.load(f)
+
+# Appearance
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("dark-blue")
+app = tkinter.Tk()
+app.geometry("720x480")
+app.title("File Organizer")
+app.configure(bg="#8daea1")
 
 
 def organize_files(folder_path):
@@ -29,7 +39,30 @@ def organize_files(folder_path):
                 shutil.move(os.path.join(folder_path, item), os.path.join(destination_folder, item))
 
 
-if __name__ == "__main__":
-    folder_path = input("Enter the folder path to organize files: ")
-    organize_files(folder_path)
-    print("Files organized successfully!")
+def organize_file():
+    try:
+        file_path = path_input.get()
+        title.configure(text=file_path, text_color="#1B4242")
+        organized.configure(text="")
+        organize_files(file_path)
+        organized.configure(text=" Organized ", text_color="green")
+    except:
+        organized.configure(text=" Oops Invalid file path ", text_color="red")
+
+
+title = customtkinter.CTkLabel(app, text=" Feed me the file path ", text_color="#1B4242")
+title.pack(padx=10, pady=10)
+
+path = tkinter.StringVar()
+path_input = customtkinter.CTkEntry(app, width=350, height=50, textvariable=path, fg_color="#EEE7DA")
+path_input.pack()
+
+
+organized = customtkinter.CTkLabel(app, text="")
+organized.pack()
+
+organize_button = customtkinter.CTkButton(app, text=" Organize ", command=organize_file, fg_color="#092635", hover_color="#1B4242")
+organize_button.pack(padx=10, pady=10)
+
+
+app.mainloop()
